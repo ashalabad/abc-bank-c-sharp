445bf3f4f19ca664396b98a17b840db80cc1f089
ashalabad <igorpol@gmail.com>
2016-07-16 19:30:15 -0400

    unit tests for DateProvider have been added

---
+++ AbcBank.Test/DateProviderTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace AbcBank.Test
+{
+    [TestFixture]
+    public class DateProviderTest
+    {
+        [Test]
+        public void TestDateProviderInstanceNotNull()
+        {
+            Assert.IsNotNull(DateProvider.getInstance());
+        }
+        [Test]
+        public void TestGettingNowDateShouldBeUTCCurrentDateTime()
+        {
+            DateProvider provider = DateProvider.getInstance();
+            Assert.IsNotNull(provider);
+            DateTime now = DateTime.UtcNow;
+            DateTime date=provider.now();
+            Assert.IsFalse(date == default(DateTime));
+            Assert.IsTrue(date.Kind == DateTimeKind.Utc);
+            Assert.AreEqual(now.Year, date.Year);
+            Assert.AreEqual(now.Month, date.Month);
+            Assert.AreEqual(now.Day, date.Day);
+            Assert.AreEqual(now.Hour, date.Hour);
+            Assert.AreEqual(now.Minute, date.Minute);
+            Assert.AreEqual(now.Hour, date.Hour);
+            Assert.AreEqual(now.Minute, date.Minute);
+            Assert.AreEqual(now.Second, date.Second);
+        }
+    }
+}
